35a2f12fc21da39c16fb40a6c6961222b9f96512
Vladchere <vladchere.ok@mail.ru>
2020-06-30 00:03:22 +0700

    Initial Commit

---
+++ MyCoreDataDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoreDataDemoApp
+//
+//  Created by Vladislav on 30.06.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
